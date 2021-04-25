f1dbae9c2211515aae239def959321ea20b1a1c8
Sergfrol33 <56001868+Sergfrol33@users.noreply.github.com>
2021-04-25 21:42:51 +0300

    add sub-menu for header

--- src/components/Header/HeaderList.jsx
+++ src/components/Header/HeaderList.jsx
@@ -13,6 +13,14 @@ const HeaderList = () => {
                                 Каталог
                                 <span>аниме</span>
                             </Link >
+                            <div className="header__sub-menu">
+                                <span className="header__sub-menu-item">
+                                    <Link to={'#'}>Все аниме</Link>
+                                </span>
+                                <span className="header__sub-menu-item"><Link to={'#'}>Анонсы</Link></span>
+                                <span className="header__sub-menu-item"><Link to={'#'}>Расписание онгоингов</Link></span>
+                                <span className="header__sub-menu-item"><Link to={'#'}>Онгоинги</Link></span>
+                            </div>
                         </li>
                         <li className={'header__list-item'}>
                             <Link to={'#'} className={'header__link'}>
@@ -30,6 +38,11 @@ const HeaderList = () => {
                             <Link to={'#'} className={'header__link'}>
                                 Сообщество
                             </Link>
+                            <div className="header__sub-menu">
+                                <span className="header__sub-menu-item"><Link to={'#'}>Рецензии</Link></span>
+                                <span className="header__sub-menu-item"><Link to={'#'}>Пользователи</Link></span>
+                                <span className="header__sub-menu-item"><Link to={'#'}>Чат</Link></span>
+                            </div>
                         </li>
                         <li className={'header__list-item'}>
                             <Link to={'#'} className={'header__link'}>
@@ -41,6 +54,14 @@ const HeaderList = () => {
                             <Link to={'#'} className={'header__link'}>
                                 Помощь
                             </Link>
+                            <div className="header__sub-menu">
+                                <span className="header__sub-menu-item">
+                                    <Link to={'#'}>FAQ</Link>
+                                </span>
+                                <span className="header__sub-menu-item"><Link to={'#'}>Правила сайта</Link></span>
+                                <span className="header__sub-menu-item"><Link to={'#'}>Информация для правообладателей из РФ</Link></span>
+                                <span className="header__sub-menu-item"><Link to={'#'}>Политика конфиденциальности</Link></span>
+                            </div>
                         </li>
                     </ul>
     );
