8d3acbfe13d13f6b40acf700013c5f4d14f418e1
Sergfrol33 <56001868+Sergfrol33@users.noreply.github.com>
2021-04-30 01:27:12 +0300

    add slider swiper

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,5 +1,94 @@
 import './slider.sass'
-const Slider = ({children}) =>{
+import image1 from '../../images/slider/image1.jpg'
+import image2 from '../../images/slider/image2.jpg'
+import image3 from '../../images/slider/image3.jpg'
+import image4 from '../../images/slider/image4.jpg'
+import image5 from '../../images/slider/image5.jpg'
+import image6 from '../../images/slider/image6.jpg'
+import image7 from '../../images/slider/image7.jpg'
+import image8 from '../../images/slider/image8.jpg'
+import image9 from '../../images/slider/image9.jpg'
+import image10 from '../../images/slider/image10.jpg'
+import image11 from '../../images/slider/image11.jpg'
+import image12 from '../../images/slider/image12.jpg'
+import image13 from '../../images/slider/image13.jpg'
+import image14 from '../../images/slider/image14.jpg'
+import image15 from '../../images/slider/image15.jpg'
+import image16 from '../../images/slider/image16.jpg'
+import {useEffect} from "react";
+const Slider = ({children,setImages}) =>{
+    useEffect(()=>{
+        setImages([
+            {
+                image: image1,
+                name: 'Название аниме',
+            },
+            {
+                image: image12,
+                name: 'Название аниме',
+            },
+            {
+                image: image2,
+                name: 'Название аниме',
+            },
+            {
+                image: image3,
+                name: 'Название аниме',
+            },
+            {
+                image: image4,
+                name: 'Название аниме',
+            },
+            {
+                image: image5,
+                name: 'Название аниме',
+            },
+            {
+                image: image6,
+                name: 'Название аниме',
+            },
+            {
+                image: image7,
+                name: 'Название аниме',
+            },
+            {
+                image: image8,
+                name: 'Название аниме',
+            },
+            {
+                image: image9,
+                name: 'Название аниме',
+            },
+            {
+                image: image10,
+                name: 'Название аниме',
+            },
+            {
+                image: image11,
+                name: 'Название аниме',
+            },
+            {
+                image: image12,
+                name: 'Название аниме',
+            },
+            {
+                image: image13,
+                name: 'Название аниме',
+            },
+            {
+                image: image14,
+                name: 'Название аниме',
+            },
+            {
+                image: image15,
+                name: 'Название аниме',
+            },
+            {
+                image: image16,
+                name: 'Название аниме',
+            },
+        ])
+    },[setImages])
     return (
         <div className={'slider'}>
             {children}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,43 @@
 import './main-page.sass'
 import Slider from "../../components/Slider/Slider";
-
+import {Swiper,SwiperSlide} from "swiper/react";
+import SwiperCore, {Navigation, Autoplay} from 'swiper/core'
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import {useState} from "react";
+import './main-page.sass'
+import {Link} from "react-router-dom";
+import cn from 'classnames'
+import arrow from '../../images/slider/arrow.svg'
 const Home = () => {
+    SwiperCore.use([Navigation, Autoplay])
+    const [images,setImages] = useState([])
     return (
         <main className={'content'}>
             <section className={'main-page'}>
                 <div className="center">
-                    <Slider>
+                    <Slider setImages={setImages}>
                         <div className="slider__title">
                             аниме весеннего сезона
                         </div>
+                        <Swiper slidesPerGroup ={4} loop autoplay navigation={{
+                            prevEl: '.arrow--prev',
+                            nextEl:'.arrow--next'
+                        }} slidesPerView={8}>
+                            {images.map((slide, index) =>{
+                              return  <SwiperSlide key={index} >
+                                        <Link to={''} className={'slider__link'} href="">
+                                            <img  className={'slider__image'} src={slide.image} alt=""/>
+                                            <span className={'slider__name'}>
+                                        {slide.name}
+                                    </span>
+                                        </Link>
+                                </SwiperSlide>
+                            })}
+                            <button title={'prev'} className={cn( 'arrow-wrapper', 'arrow-wrapper--prev')}><img src={arrow} className={'arrow arrow--prev'} alt=""/></button>
+                            <button title={'next'} className={cn( 'arrow-wrapper',  'arrow-wrapper--next')}><img src={arrow} className={'arrow arrow--next'} alt=""/></button>
+                        </Swiper>
                     </Slider>
                 </div>
             </section>
