3308c366aae9e6ae825794a7319c4b1bbc4a50d4
sergfrol33 <sergfrol33@gmail.com>
2021-05-26 12:38:00 +0300

    login form on useRef

--- src/components/Top/LoginBlock.jsx
+++ src/components/Top/LoginBlock.jsx
@@ -1,24 +1,18 @@
 import './top.sass'
 import cn from "classnames";
 import {Link} from "react-router-dom";
-import React, {useState} from "react";
+import React, {useRef} from "react";
 const LoginBlock = () => {
-    const [fade,setFade] = useState(false)
-    const loginHandler = () => {
-        return setFade(value => !value) // переключатель формы
-    }
+    const loginForm = useRef(null)
     return (
         <div className="top__login-block">
-            <button onClick={() => loginHandler()} className={'top__button top__button--login'}>Вход</button>
-            {fade ? <form className= {cn({
-                'top__login-form':'top__login-form',
-                'top__login-form--fade': fade && 'top__login-form--fade'
-            })}>
+            <button onClick={() => loginForm.current.classList.toggle('top__login-form--fade')} className={'top__button top__button--login'}>Вход</button>
+           <form ref={loginForm} className= {cn({'top__login-form': 'top__login-form'})}>
                 <input placeholder={'E-mail'} type="text" className="top__login-input"/>
                 <input placeholder={'Пароль'}  type="password" className="top__login-input"/>
                 <button className={'top__login-button'}>Войти</button>
                 <Link className={'top__login-reset-password'} to={'#'}>Не помню пароль...</Link>
-            </form> : null
+            </form>
             }
         </div>
     );
